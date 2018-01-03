7ee4e2f98ef81d4696e2125c28bffb883296debd
Christina Collura <cmcollura@gmail.com>
2018-01-03 01:38:17 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
   if array.length >= 4
-    return array.join()
+    array.join()
   eslif array.length == 2
-    return array.join(" and ")
+    array.join(" and ")
   eslif array.length == 3
     return array.join(", ",", "," and ")
   else 
-    return array.join
+    array.join
   end
 end
